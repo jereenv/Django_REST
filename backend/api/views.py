c0777ad60ff60a83425dcc22d4ce5abe28020fd4
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-15 04:16:19 -0700

    Using REST response instead of JsonResponse

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,20 @@
 import json
-from django.http import JsonResponse, HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from django.forms.models import model_to_dict
 
 from products.models import Product
 
-
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'price'])
-    return JsonResponse(data)
+    return Response(data)
