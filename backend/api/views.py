5ed29e992823bcbc61bd32abf4a5fce674e2f519
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-15 03:30:57 -0700

    echoing data back to client

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,18 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API Response!!"})
+
+
+    print(request.GET) #url query params
+    body = request.body #byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON Data -> Python Dict
+    except:
+        pass
+    print(data.keys())
+    data['params']  = dict(request.GET)
+    data['headers']  = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={"abc": 123} ,json={'query': 'Hello World'})
 
-print(get_response.json())
+print(get_response.text)
