6fa64580e40f2bc1929ca3281afbe48a22d18007
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-15 03:54:07 -0700

    passing django model instances as API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,15 @@
-from django.http import JsonResponse
 import json
+from django.http import JsonResponse
+
+from products.models import Product
+
 
 def api_home(request, *args, **kwargs):
-
-
-    print(request.GET) #url query params
-    body = request.body #byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON Data -> Python Dict
-    except:
-        pass
-    print(data.keys())
-    data['params']  = dict(request.GET)
-    data['headers']  = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123} ,json={'query': 'Hello World'})
+get_response = requests.get(endpoint ,json={'product_id': 123})
 
-print(get_response.text)
+print(get_response.json())
